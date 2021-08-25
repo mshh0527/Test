92b33d5ab662a24c2165c043db7c3b525f9a80cf
kms <mshh0527@naver.com>
2021-08-25 16:50:03 +0900

    Commit Test

---
+++ Desktop/3-2/Java(2021)/1주차실습(2)/src/Calendar.java
@@ -0,0 +1,26 @@
+/*
+ * 2021년 9월 달력만들기
+ */
+
+public class Calendar {
+	
+	public static void main(String[]args) {
+		String[]days = {"일","월","화","수","목","금","토"};
+		final int NUM = 30;
+		
+		System.out.println("2021년 9월");
+		
+		for(int i=0;i<7;i++) {
+			System.out.print(days[i]+"\t");
+		}
+		System.out.print("\n");
+		
+		for(int i=1;i<=NUM;i++) {
+			System.out.print(i+"\t");
+			if(i%7==0)
+				System.out.println();
+		}
+		
+	}
+
+}
